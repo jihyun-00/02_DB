d4d204b7f4577cc6ea369b261c37d7a95472f02a
jihyun-00 <rkdwl811@naver.com>
2025-03-26 08:56:52 +0900

    스터디 테이블

--- study.sql
+++ study.sql
@@ -18,4 +18,26 @@ CREATE TABLE TB_STUDENT(
 );
 
 
--- 학생은 한학기에 최대 10학점까지만 수강 할 수 있다.
+-- 학과는 학과번호를 부여하여 식별하며 학과명, 학과전화번호, 학과사무실 정보를 가진다. 해당 학과에서 개설하는 강좌가 반드시 1개 이상 존재해야 하며, 동시에 학과장이 1명 있어야 한다.
+
+CREATE TABLE TB_DEPARTMENT(
+		DEPT_COD VARCHAR2(45) PRIMARY KEY,
+		DEPT_TITLE VARCHAR2(45) NOT NULL,
+		DEPT_TEL VARCHSR2(45) NOT NULL,
+		DEPT_OFFICE VARCHAR2(45),
+		PROF_BOSS VARCHAR2(45) NOT NULL
+);
+
+-- 강좌테이블에서 학과코드 참조
+
+-- 강좌는 강좌번호, 분반번호, 강의하는 교수, 강좌이름, 강의요일, 강의교시, 취득학점 (1~4), 강좌시간 (1~6), 개설 학과, 강의실 정보가 필요하다.
+
+CREATE TABLE TB_LECTURE(
+		LCT_ID VARCHSR2(45) PRIMARY KEY,
+		CLASS_NO NUMBER,
+		PROF_ID VARCHAR2(45),
+		CLASS_NAME VARCHAR(45),
+		CLASS_DAY VARCHAR2(45),
+		
+		
+)
